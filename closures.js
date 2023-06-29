c702fbfa49c6397480b07bfc5145caa631968626
Afan Khan <mrkhanads@gmail.com>
2023-06-29 11:10:16 +0530

    Added labelled comments

--- closures.js
+++ closures.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* Introduction Example */
+
 let numOfReaders = 5;
 
 const newReaderCount = () => {
@@ -16,11 +18,26 @@ readers();
 
 console.dir(readers);
 
+/* Multiplier Example (Partial reference from Eloquent JS) */
+
+function multiplier(factor) {
+  return function (number) {
+    console.log(number * factor);
+  };
+}
+
+const twice = multiplier(2);
+twice(5); // 10
+
+/* Interview Question Snippet */
+
 for (let i = 0; i < 3; i++) {
   setTimeout(function log() {
     console.log(i);
   }, 1000);
 }
+
+/* Basic Usage Example */
 
 const nameOfAuthor = "Afan Khan";
 
